2dc3b2f7a1c1abdb29d85b74b5fa82ab343c3114
Shining143 <ahmadslb2293@gmail.com>
2023-09-19 03:01:18 +0530

    adding new code in Test5

---
+++ src/test/java/classes/Test5.java
@@ -0,0 +1,33 @@
+package classes;
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategy;
+
+public class Test5 {
+	
+	@Test
+	public void method() throws Exception {
+		
+		RemoteWebDriver driver = new ChromeDriver();
+		driver.get("https://shop.polymer-project.org/");
+		driver.manage().window().maximize();
+		// full page screenshot
+		AShot as = new AShot();
+		ShootingStrategy shs = ShootingStrategies.viewportPasting(1000);
+		Screenshot ss = as.shootingStrategy(shs).takeScreenshot(driver);
+		File dest = new File("target/fullpagescreenshot.png");
+		ImageIO.write(ss.getImage(), "PNG", dest);
+		
+	} 
+
+}
